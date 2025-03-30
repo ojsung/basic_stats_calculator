541c0f8cd16b048c48bf193800ec1cb1a7f732e9
ojsung <jacob.sung@lendio.com>
2025-03-30 17:25:11 -0600

    Implement math_utils.Pow - Create equality test method for binaryExp - Add binaryPowEven and binaryPowOdd to handle even vs odd powers - Update precision of float to 256, and return it at 128 with rounding

--- internal/math_utils/big_float_helpers.go
+++ internal/math_utils/big_float_helpers.go
@@ -5,14 +5,104 @@ import (
 	"fmt"
 	"math/big"
 )
+type binaryExp struct {
+	r *big.Float
+	a *big.Float
+	x *big.Int
+}
+func (b binaryExp) String() string {
+	return fmt.Sprintf("r: %v\na: %v\nx: %v\n", b.r, b.a, b.x)
+}
+
+func (a binaryExp) Equal(b binaryExp) (isEqual bool, failed binaryExpComparison) {
+	compareA := a.a.Cmp(b.a) == 0
+	compareR := a.r.Cmp(b.r) == 0
+	compareX := a.x.Cmp(b.x) == 0
+	var binComparison binaryExpComparison
+	if !compareA {
+		binComparison.a = comparison[*big.Float]{
+			own:   a.a,
+			other: b.a,
+		}
+	}
+	if !compareR {
+		binComparison.r = comparison[*big.Float]{
+			own: a.r,
+			other: b.r,
+		}
+	}
+	if !compareX {
+		binComparison.x = comparison[*big.Int]{
+			own: a.x,
+			other: b.x,
+		}
+	}
+	return a.a.Cmp(b.a) == 0 && a.r.Cmp(b.r) == 0 && a.x.Cmp(b.x) == 0, binComparison
+}
+
+type comparison[T any] struct {
+	own T
+	other T
+}
+type binaryExpComparison struct {
+	r comparison[*big.Float]
+	a comparison[*big.Float]
+	x comparison[*big.Int]
+}
+
+const Ln2String = "0.6931471805599453"
+var zero *big.Int = big.NewInt(0)
+var one *big.Int = big.NewInt(1)
+var two *big.Int = big.NewInt(2)
+var floatTwo *big.Float = new(big.Float).SetInt64(2)
 
 func BigFloatFromString(value string) *big.Float {
-	f, _ := new(big.Float).SetString(value)
+	f, _ := precFloat().SetString(value)
 	return f
 }
 
-func Pow(base *big.Float, exponent *big.Float) (power *big.Float, err error) {
-	return
+func Pow(base *big.Float, exponent *big.Int) (power *big.Float) {
+	composition := binaryExp{
+		BigFloatFromString("1"),
+		base.SetPrec(128),
+		exponent,
+	}
+	i := new(big.Int).Set(composition.x)
+	for i.Cmp(zero) == 1 {
+		if (new(big.Int).Mod(i, two).Cmp(one) == 0) {
+			composition = binaryPowOdd(composition)
+		} else {
+			composition = binaryPowEven(composition)
+		}
+		i = composition.x
+	}
+
+	return normalizeReturn(composition.r)
+}
+
+func binaryPowEven(binaryPow binaryExp) (evenPow binaryExp) {
+	return binaryExp{
+		binaryPow.r,
+		precFloat().Mul(binaryPow.a, binaryPow.a),
+		new(big.Int).Div(binaryPow.x, two),
+	}
+}
+
+func binaryPowOdd(binaryPow binaryExp) (oddPow binaryExp) {
+	r := precFloat().Mul(binaryPow.a, binaryPow.r)
+	return binaryPowEven(binaryExp{
+		r,
+		binaryPow.a,
+		binaryPow.x,
+	})
+}
+
+func precFloat() *big.Float {
+	return new(big.Float).SetPrec(256).SetMode(big.ToNearestEven)
+}
+
+func normalizeReturn(value *big.Float) *big.Float {
+	return value.SetMode(big.ToZero).SetPrec(128)
 }
 
 func NaturalPow(exponent *big.Float) (power *big.Float, err error) {
@@ -27,8 +117,12 @@ func taylorApproximationLn(argument *big.Float) (logarithm *big.Float, err error
 	if argument.Sign() <= 0 {
 		return nil, errors.New("log is only defined for numbers greater than 0")
 	}
-	if new(big.Float).Abs(new(big.Float).Sub(argument, new(big.Float).SetInt64(1))).Cmp(new(big.Float).SetInt64(1)) < 1 {
-		return nil, errors.New(fmt.Sprintf("Taylor expansion appx of ln only converges for values (s + 1), s <= 1, received %v", argument))
+	if precFloat().Abs(precFloat().Sub(argument, precFloat().SetInt64(1))).Cmp(precFloat().SetInt64(1)) < 1 {
+		return nil, fmt.Errorf("taylor expansion appx of ln only converges for real values (s + 1), where s <= 1, received %v", argument)
 	}
+	// If a function f(x) is infinitely differentiable at a point (a), its approximation, given by the Taylor expansion series, about that point is
+	// given by f(x) = (f(a) * (x-a)^0)/0! + (f'(a) * (x-a)^1)/1! + (f''(a) * (x-a)^2)/2! + (f'''(a) * (x-a)^3)/3! + ... + Σ [(-1)^(n+1) * (x - 1)^n] / n, n=1 to ∞
+	// For f(x) = ln(x), f'(x) = (1/x), f''(x) = -(1/x^2), etc..
+	
 	return
 }
--- internal/math_utils/big_float_helpers_test.go
+++ internal/math_utils/big_float_helpers_test.go
@@ -1,6 +1,7 @@
 package math_utils
 
 import (
+	"fmt"
 	"math/big"
 	"reflect"
 	"testing"
@@ -16,7 +17,7 @@ func Test_bigFloatFromString(t *testing.T) {
 			"It should parse decimal values",
 			"0.0123",
 			func() *big.Float {
-				f, _ := new(big.Float).SetString("0.0123")
+				f, _ := precFloat().SetString("0.0123")
 				return f
 			}(),
 		},
@@ -24,7 +25,7 @@ func Test_bigFloatFromString(t *testing.T) {
 			"It should parse mixed values",
 			"123.456",
 			func() *big.Float {
-				f, _ := new(big.Float).SetString("123.456")
+				f, _ := precFloat().SetString("123.456")
 				return f
 			}(),
 		},
@@ -41,21 +42,99 @@ func Test_bigFloatFromString(t *testing.T) {
 func TestPow(t *testing.T) {
 	type args struct {
 		base     *big.Float
-		exponent *big.Float
+		exponent *big.Int
 	}
 	tests := []struct {
 		name string
 		args args
 		want *big.Float
 	}{
-		// TODO: Add test cases.
+		{
+			"It should return 14348907 for 3^15",
+			args{
+				precFloat().SetInt64(3),
+				big.NewInt(15),
+			},
+			precFloat().SetInt64(14348907),
+		},
+		{
+			"It should return 0.32768 for 0.8^5",
+			args{
+				BigFloatFromString("0.8"),
+				big.NewInt(5),
+			},
+			BigFloatFromString("0.32768"),
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got, err := Pow(tt.args.base, tt.args.exponent); err != nil {
-				t.Errorf("Pow() error = %v", err)
-			} else if !reflect.DeepEqual(got, tt.want) {
+			got := Pow(tt.args.base, tt.args.exponent)
+			got.SetMode(big.ToNearestEven).SetPrec(128)
+			tt.want.SetMode(big.ToNearestEven).SetPrec(128)
+			if got.Cmp(tt.want) != 0 {
 				t.Errorf("Pow() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_binaryPowEven(t *testing.T) {
+	tests := []struct {
+		name        string
+		binaryPow   binaryExp
+		wantEvenPow binaryExp
+	}{
+		{
+			"It should return the binary exponent form of 6^4 as 36^2 when r=1",
+			binaryExp{
+				precFloat().SetInt64(1),
+				precFloat().SetInt64(6),
+				big.NewInt(4),
+			},
+			binaryExp{
+				precFloat().SetInt64(1).SetMode(big.ToZero).SetPrec(128),
+				precFloat().SetInt64(36).SetMode(big.ToZero).SetPrec(128),
+				big.NewInt(2),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotEvenPow := binaryPowEven(tt.binaryPow);
+			equal, failed := gotEvenPow.Equal(tt.wantEvenPow)
+			if !equal {
+				t.Errorf("comparison failed. a: %v, r: %v, x: %v", failed.a, failed.r, failed.x)
+			}
+		})
+	}
+}
+
+func Test_binaryPowOdd(t *testing.T) {
+	tests := []struct {
+		name       string
+		binaryPow  binaryExp
+		wantOddPow binaryExp
+	}{
+		{
+			"It should return r = 27, a = 81, x = 3 for 3 * 9^7",
+			binaryExp{
+				precFloat().SetInt64(3),
+				precFloat().SetInt64(9),
+				big.NewInt(7),
+			},
+			binaryExp{
+				precFloat().SetInt64(27).SetMode(big.ToZero).SetPrec(128),
+				precFloat().SetInt64(81).SetMode(big.ToZero).SetPrec(128),
+				big.NewInt(3),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotOddPow := binaryPowOdd(tt.binaryPow);
+			equal, failed := gotOddPow.Equal(tt.wantOddPow)
+			if !equal {
+				t.Errorf("comparison failed. a: %v, r: %v, x: %v", failed.a, failed.r, failed.x)
 			}
 		})
 	}
@@ -85,9 +164,9 @@ func TestNaturalPow(t *testing.T) {
 
 func Test_Ln(t *testing.T) {
 	tests := []struct {
-		name string
+		name     string
 		argument *big.Float
-		want *big.Float
+		want     *big.Float
 	}{
 		// TODO: Add test cases.
 	}
@@ -116,15 +195,22 @@ func Test_taylorApproximationLn(t *testing.T) {
 			true,
 		},
 		{
-			"It should error for negative numbers",
+			"It should error for real negative numbers",
 			big.NewFloat(-1.0),
 			nil,
 			true,
 		},
 		{
-			"It should return a a value for positive, real numbers (argument = 4)",
-			new(big.Float).SetInt64(4),
-			BigFloatFromString("1.3862943611198906"),
+			"It should error for real numbers greater than 1",
+			big.NewFloat(4.0),
+			nil,
+			true,
+		},
+		{
+			"It should return a a value for r with domain (0, 1) in Reals (argument = 0.5)",
+			precFloat().SetFloat64(0.5),
+			// ln(0.5) = ln(1/2) = ln(2^-1) = -1 * ln(2) = -ln(2)
+			BigFloatFromString(fmt.Sprintf("-%v", Ln2String)),
 			false,
 		},
 	}
